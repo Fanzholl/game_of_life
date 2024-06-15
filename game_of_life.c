6ee32b6dfd6c5266f59059af812e0dc48cf814f9
Fedorov Mitriy <58570733+Fanzholl@users.noreply.github.com>
2024-06-15 23:42:43 +0300

    develop: impotent changes for structure a programm of Game Of Life file.

--- game_of_life.c
+++ game_of_life.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void initializeGridData(int grid[25][80], int size[2]);
+void printGrid(int grid[25][80], int size[2]);
 
 int main(void) {
+    int grid[25][80]; // Инициализация массива сетки поля.
+    int nextGrid[25][80]; // Инициализация массива сетки следуещего шага эволюции игрового поля.
+    int size[2] = {25, 80}; // Инициализация массива с данными, о размере массива сетки.
+
 
 
     return 0;
+}
+
+void initializeGridData(int grid[25][80], int size[2]) {
+    for (int i = 0; i < size[0]; i++) {
+        for (int j = 0; j < size[1]; j++) {
+            grid[i][j] = rand() % 2;
+        }
+    }
+}
+
+void printGrid(int grid[25][80], int size[2]) {
+    for (int i = 0; i < size[0]; i++) {
+
+    }
 }
